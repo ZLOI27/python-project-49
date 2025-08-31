bfd936068207ae64d6506639f1ea21510fe42d43
Kirill Zlobinov <zlobinovk@yandex.ru>
2025-09-01 00:50:45 +0300

    add: brain_gcd.py and gcd.py

--- Makefile
+++ Makefile
@@ -18,7 +18,9 @@ lint:
 	uv run ruff check brain_games/scripts/brain_games.py
 	uv run ruff check brain_games/scripts/brain_even.py
 	uv run ruff check brain_games/scripts/brain_calc.py
+	uv run ruff check brain_games/scripts/brain_gcd.py
 	uv run ruff check brain_games/games/even.py
 	uv run ruff check brain_games/games/calc.py
+	uv run ruff check brain_games/games/gcd.py
 	uv run ruff check brain_games/utils.py
 	uv run ruff check brain_games/engine.py
---
+++ brain_games/games/gcd.py
@@ -0,0 +1,30 @@
+import random
+
+from brain_games.utils import ask_answer
+
+
+def get_answer_and_right_answer(expression: str) -> tuple:
+    answer = ask_answer()
+    right_answer = get_right_answer(expression)
+    return (answer, right_answer)
+
+
+def get_question_msg() -> str:
+    left_num = random.randint(0, 100)
+    right_num = random.randint(0, 100)
+    return f"{left_num} {right_num}"
+
+
+def get_right_answer(expression: str) -> str:
+    left_num, right_num = expression.split()
+    left_num = int(left_num)
+    right_num = int(right_num)
+    while right_num != 0:
+        mod = left_num % right_num
+        left_num = right_num
+        right_num = mod
+    return str(left_num)
+
+
+def get_msg_game_rules() -> str:
+    return "Find the greatest common divisor of given numbers."
